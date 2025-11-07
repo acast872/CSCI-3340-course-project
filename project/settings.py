4d204c186784b8e71761a15deac2d5fc5b53d48d
acast872 <140276833+acast872@users.noreply.github.com>
2025-11-07 03:46:37 +0000

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -80,12 +80,9 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-<<<<<<< HEAD
-=======
         'OPTIONS': {
             'timeout' : 10,
         }
->>>>>>> SilverRed-init
     }
 }
 
