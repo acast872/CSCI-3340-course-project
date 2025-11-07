7c68381d322f5a7c8d529fe3419a2cc49b17965b
GilbertoGarza <12262002g@gmail.com>
2025-11-07 12:31:54 -0600

    Update urls.py

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,1 +1,9 @@
+from django.urls import path
+from . import views
 
+urlpatterns = [
+    path('rooms/', views.room_list, name='room_list'),
+    path('rooms/<int:room_id>/reserve/', views.reserve_room, name='reserve_room'),
+    path('my-reservations/', views.my_reservations, name='my_reservations'),
+    path('approve/<int:res_id>/', views.approve_reservation, name='approve_reservation'),
+]
