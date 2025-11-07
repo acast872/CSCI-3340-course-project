a140cfcf5e6aa7bdb9635b69b4f8ad613d90cc52
acast872 <140276833+acast872@users.noreply.github.com>
2025-11-07 03:47:02 +0000

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -37,10 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-<<<<<<< HEAD
-=======
     'accounts',
->>>>>>> SilverRed-init
 ]
 
 MIDDLEWARE = [
